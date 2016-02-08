afebed3bfa3e3217f438ec05dd4c3c3476231f3c
bjornhall <bjornhalldor@gmail.com>
2016-02-08 23:32:36 +0000

    intial commit

---
+++ Parser.py
@@ -0,0 +1,8 @@
+import sys
+
+# Implement the class Parser, the syntax analyzer (parser). This should be a top-down recursive- descent parser for the grammar G above.
+# The output of the parser is the stack-based inter- mediate code S, corresponding to the given program, written to standard output (stdout).
+# If the expression is not in the language (or if an ERROR token is returned by the Lexer) then the parser should output the error message
+# “Syntax error!” (at the point when the error is recognized) and immediately quit.
+# The parser should have at least two (private) member variables, one of type Lexer, the other of type Token (for the current token).
+# It should only have a single public method, parse(), for initiating the parse – other methods are private.
---
+++ Token.py
@@ -0,0 +1,5 @@
+# Implement the class Token, which contains both a lexeme and a token code:
+#          String lexeme;
+#          TokenCode tCode;
+# where TokenCode is:
+# enum TokenCode { ID, ASSIGN, SEMICOL, INT, ADD, SUB, MULT, LPAREN, RPAREN, PRINT, END, ERROR }
