fb8ddccc1a2f9038e428bd7c2b8f211b8a68b519
Ásgeir Másson <asgeirthor@gmail.com>
2016-02-11 22:59:51 +0000

    Debug Interpreter.py

--- Interpreter.py
+++ Interpreter.py
@@ -15,41 +15,33 @@ def stackpop(op):
         sys.exit()
 
 # A method for parsing the input correctly before performing a operation
-def checkInput(input1, input2):
-    variables = [];
-    if isinstance(input1, str):
-        if input1.isdigit():
-            variables.append(int(input1))
+def checkInput(myInput):
+    if isinstance(myInput, str):
+        if myInput.isdigit():
+            myInput = (int(myInput))
         else:
-            variables.append(int(map[input1]))
-    if isinstance(input2, str):
-        if input2.isdigit():
-            variables.append(int(input2))
-        else:
-            variables.append(int(map[input2]))
-    return variables
+            myInput = (int(map[myInput]))
+    return myInput
+
 
 # A loop that goes through each input line and handles the operator in it accordingly
 for inputLine in sys.stdin:
-    inputLine = InputLine.rstrip()
+    inputLine = inputLine.rstrip()
     if inputLine[0:4] == "PUSH":
         value = inputLine[5:]
         S.append(value)
     elif inputLine[0:3] == "ADD":
         input1 = stackpop("ADD")
         input2 = stackpop("ADD")
-        variables = checkInput(input1, input2)
-        S.append(variables.pop() + variables.pop())
+        S.append(checkInput(input1) + checkInput(input2))
     elif inputLine[0:3] == "SUB":
         input1 = stackpop("SUB")
         input2 = stackpop("SUB")
-        variables = checkInput(input1, input2)
-        S.append(variables.pop() - variables.pop())
+        S.append(checkInput(input2) - checkInput(input1))
     elif inputLine[0:4] == "MULT":
         input1 = stackpop("MULT")
         input2 = stackpop("MULT")
-        variables = checkInput(input1, input2)
-        S.append(variables.pop() * variables.pop())
+        S.append(checkInput(input1) * checkInput(input2))
     elif inputLine[0:6] == "ASSIGN":
         val = stackpop("ASSIGN")
         var = stackpop("ASSIGN")
